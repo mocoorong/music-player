316fb14413268c0d281c780176e3c7bcb37afb8c
Hajun Jeon <gkwns1231@naver.com>
2026-01-11 22:59:07 +0900

    메인 화면 기본 구조 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+'use client'
+
+import {useState} from 'react'
+import './page.css'
+
 export default function () {
+  const [play, setPlay] = useState(false)
+
   return (
-    <div>
-      <h1>Music Player</h1>
+    <div className="main-bg">
+      <div className="music-playlist-add">
+        <div className="plus-btn"></div>
+      </div>
+      <div className="music-var">
+        <div className="music-var-title">노래 제목 들어갈 곳</div>
+        <div className="progress-container">
+          <div className="progress-bar"></div>
+        </div>
+        <audio id="audio" src="/music/sample.mp3"></audio>
+        <div className="control-btns">
+          <button>
+            <img src={'/img/main-prevBtn.png'}></img>
+          </button>
+          <button onClick={() => setPlay(!play)}>
+            <img
+              src={play ? '/img/main-pauseBtn.png' : '/img/main-playBtn.png'}
+            ></img>
+          </button>
+          <button>
+            <img src={'/img/main-nextBtn.png'}></img>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }

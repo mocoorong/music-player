10019d20156843c96d78d4f8da8f879aa78c8db5
Hajun Jeon <gkwns1231@naver.com>
2026-04-17 04:31:07 +0900

    구글 로그인 추가

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,13 @@
 import Kakao from 'next-auth/providers/kakao'
+import Google from 'next-auth/providers/google'
 import type {NextAuthConfig} from 'next-auth'
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Kakao({
       clientId: process.env.KAKAO_CLIENT_ID,
       clientSecret: process.env.KAKAO_CLIENT_SECRET,
